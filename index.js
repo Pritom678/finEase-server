8058baf81e9869f4bccfb9a1c07fb41aa714ad89
Pritom678 <152401917+Pritom678@users.noreply.github.com>
2026-01-03 23:40:32 +0600

    something change

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("finEase-DB");
-    const expenseCollection = db.collection("expenses");
+    const expenseCollection = db.collection("expense");
 
     // view all data
     app.get("/transactions", async (req, res) => {
@@ -237,7 +237,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
